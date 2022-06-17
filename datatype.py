cb24dc7ec635583db2cf72ca26795c5954b63918
Lingga Maulana <linggakum@gmail.com>
2022-06-17 08:52:42 +0700

    initial commit

---
+++ datatype.py
@@ -0,0 +1,47 @@
+# TUPLE
+'''
+Tuple
+Tuples are used to store multiple items in a single variable.
+
+Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary, all with different qualities and usage.
+
+A tuple is a collection which is ordered and unchangeable.
+
+Tuples are written with round brackets.
+tuple (123,"abc")
+'''
+
+
+
+#LIST
+'''
+List
+Lists are used to store multiple items in a single variable.
+
+Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+
+Lists are created using square brackets:
+list [123, "abc"]
+'''
+
+# DICTIONARY
+'''
+Dictionary
+Dictionaries are used to store data values in key:value pairs.
+
+A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+dictionary {'number': 123, 'alphabet':"abc"}
+'''
+
+
+
+#SET
+'''
+Sets are used to store multiple items in a single variable.
+Set is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Tuple, and Dictionary, all with different qualities and usage.
+
+A set is a collection which is unordered, unchangeable*, and unindexed.
+set {123, "abc"}
+'''
+
+
